fc62d42f8774f7eb33b54549955061597591c81c
Ira Rainey <ira.rainey@microsoft.com>
2024-12-17 10:33:06 +0000

    Refactored logic

--- src/main.py
+++ src/main.py
@@ -5,13 +5,12 @@ from markitdown import MarkItDown
 from geopy.geocoders import AzureMaps
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 
+# Convert degrees, minutes, seconds to decimal degrees
 def dms_to_decimal(dms_str):
-    import re
-
-    # Regular expression to extract degrees, minutes, seconds, and direction
     pattern = r"(\d+) deg (\d+)' (\d+\.?\d*)\"? ([NSEW])"
     matches = re.findall(pattern, dms_str)
 
@@ -33,29 +32,42 @@ def dms_to_decimal(dms_str):
     return decimal_coords
 
 
-directory = "images"
+# Parse images in a directory to extract metadata
+def parse_images(directory):
+    markitdown = MarkItDown()
+    output = []
+    pattern = os.path.join(directory, '**', '*.jpg')
+    jpg_files = glob.glob(pattern, recursive=True)
 
-markitdown = MarkItDown()
+    for jpg_file in jpg_files:
+        result = markitdown.convert(jpg_file)
 
-pattern = os.path.join(directory, '**', '*.jpg')
-jpg_files = glob.glob(pattern, recursive=True)
+        date_pattern = r"DateTimeOriginal:\s*([^\n]+)"
+        when_taken = re.search(date_pattern, result.text_content)
 
-for jpg_file in jpg_files:
-    result = markitdown.convert(jpg_file)
+        gps_pattern = r"GPSPosition:\s*([^\n]+)"
+        gps_position = re.search(gps_pattern, result.text_content)
+        decimal_coords = dms_to_decimal(gps_position.group(1))
 
-    date_pattern = r"DateTimeOriginal:\s*([^\n]+)"
-    when_taken = re.search(date_pattern, result.text_content)
+        geolocator = AzureMaps(os.getenv("AZURE_MAPS_KEY"))
+        location = geolocator.reverse(
+            f"{decimal_coords[0]}, {decimal_coords[1]}")
 
-    gps_pattern = r"GPSPosition:\s*([^\n]+)"
-    gps_position = re.search(gps_pattern, result.text_content)
-    decimal_coords = dms_to_decimal(gps_position.group(1))
+        output.append({"filename": jpg_file,
+                       "location": {
+                           "latitude": decimal_coords[0],
+                           "longitude": decimal_coords[1],
+                           "address": location.address,
+                       },
+                       "when": when_taken.group(1)})
 
-    geolocator = AzureMaps(os.getenv("AZURE_MAPS_KEY"))
-    location = geolocator.reverse(
-        f"{decimal_coords[0]}, {decimal_coords[1]}")
+        sorted_by_date = sorted(output, key=lambda x: x["when"])
+    return sorted_by_date
 
-    print(jpg_file)
-    print(location.address)
-    print(f"{decimal_coords[0]}, {decimal_coords[1]}")
-    print(when_taken.group(1))
-    print()
+
+if __name__ == "__main__":
+
+    image_data = parse_images("images")
+
+    for item in image_data:
+        print(item)
