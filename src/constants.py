3dab52cef18e23a1f0068c801899a273c35e2bdf
Ira Rainey <ira.rainey@microsoft.com>
2024-12-18 11:12:12 +0000

    Corrected typos in prompt

--- src/constants.py
+++ src/constants.py
@@ -8,8 +8,8 @@ SUMMARY_SYSTEM_PROMPT = """
                 Create the output in markdown format and only output the markdown content.
                 The markdown created must follow these rules:
                 - Contain a top-level heading with the year or year span of the trips.
-                - Order the trips chronologically by date taken of all images for that trip to a country within now more
-                then a fourteen day period and create a separate sub-heading and section for each trip.
+                - Order the trips chronologically by date taken of all images for that trip to a country within no more
+                than a fourteen day period and create a separate sub-heading and section for each trip.
                 - Create sub-headings for each trip with the country of the location and the dates of that trip.
                 - Every trip must include, below the sub-heading, a summary of the entire trip to that location
                 referencing all the images. This summary should be descriptive and more than a single sentence.
