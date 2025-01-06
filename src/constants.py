fd6376cb20e2fcd6f40951dab309fa5786eb67f6
Ira Rainey <ira.rainey@microsoft.com>
2025-01-06 09:54:38 +0000

    Amended prompts

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,8 @@ GPT_API_VERSION = "2024-08-01-preview"
 IMAGE_SYSTEM_PROMPT = """
     You are an individual looking back at a trip you have taken by reviewing your photographs.
     Write a paragraph for this image that describes the scene.
-    Only use the latitude and longitude coordinates ({latitude},{longitude})
-    and the town or city from the address ({address}) as a reference for its geographic location.
+    Only use the latitude and longitude coordinates ({latitude},{longitude}) and the town or \
+    city from the address ({address}) as a reference for its geographic location.
     If you cannot determine the city or town then do not make it up just exclude it.
     Do not include the coordinates in the output.
     Do not include the address or the name of the city or the town in the summary.
@@ -18,42 +18,44 @@ IMAGE_SYSTEM_PROMPT = """
 # User prompt for the image description call
 IMAGE_USER_PROMPT = """
     Create a summary description for this image.
-    Use this additional context where relevant for information to describe the image as part of a trip:
-    ```text
+    Use the additional context delimited by triple backticks where relevant for information to describe the image \
+    as part of a trip:
+    ```
     {context}
     ```
     """
 
 # System prompt for the overall summary call
 SUMMARY_SYSTEM_PROMPT = """
-    You are an individual looking back at all trips you have taken and writing a journal summary from descriptions
+    You are an individual looking back at all trips you have taken and writing a journal summary from descriptions \
     of each image and relevant geographic and historic country information.
-    Create an overall summary of each trip taken by combining the caption from each image any relevant geographical
+    Create an overall summary of each trip taken by combining the caption from each image any relevant geographical \
     or historical country information together with any additional context for that trip as provided.
     Only output a summary for each trip as a single paragraph.
-    Output plain text only.
+    Only output plain text.
     Do not include the image captions.
     Do not duplicate information that is already in the image captions.
     """
 
 # User prompt for the overall summary call
 SUMMARY_USER_PROMPT = """
-    Given the following JSON journey data, collate information from each image caption to create a
-    summary of each trip.
-    ```json
+    Collate information from each image caption to create a summary of each trip using the the JSON journey data \
+    delimited by triple exclamation marks.
+    !!!
     {trip_data_json}
+    !!!
+    Use the additional context delimited by triple backticks where relevant for information to describe the image \
+    as part of a trip:
     ```
-    Use this additional context where relevant to provide add information for the trip:
-    ```text
     {context}
     ```
     """
 
 # Image summary temperature
-IMAGE_SUMMARY_TEMP = 0.3
+IMAGE_SUMMARY_TEMP = 0.2
 
 # Trip summary temperature
-TRIP_SUMMARY_TEMP = 0.3
+TRIP_SUMMARY_TEMP = 0.2
 
 # Output directory
 OUTPUT_DIR = "output"
